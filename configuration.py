3be9401a53858f9b793d11d9ac5e53c6ad7340f3
avitsov <armsarkristinf7289@gmail.com>
2024-02-09 06:57:17 +0000

    Задание на SQL и автотест

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = ("https://2dd10847-5ba8-4c5b-95f2-be372350e684.serverhub.praktikum-services.ru")
+CREATE_COURIER = "/api/v1/courier"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Баранова",
+    "lastName": "Кристина",
+    "address": "Neva, 137 apt.",
+    "metroStation": 204,
+    "phone": "+7 921 365 76 67",
+    "rentTime": 5,
+    "deliveryDate": "2024-02-02",
+    "comment": "Привет!",
+    "color": [
+        "BLACK"
+    ]
+}
